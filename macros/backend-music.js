1560450cc67a82123ab8cac137fdea665c5b5663
Archie Lamb <git@archielamb.uk>
2021-11-13 22:05:52 +0000

    Initial commit of macros in current state

---
+++ macros/backend-music.js
@@ -0,0 +1,28 @@
+const type = args[0]
+const details = args[1]
+const playlist = game.collections.getName('Playlists').getName(details)
+
+if (!(type && details && playlist)) {
+  console.log('Error setting inital variables in backend-music', args, playlist)
+  ChatMessage.create({ content: "[Macro backend-music] Passed parameters not recognised or playlist doesn't exist" })
+  return
+}
+
+switch (type) {
+  case 'playlist-toggle':
+    if (playlist.playing) {
+      playlist.stopAll()
+    } else {
+      playlist.playAll()
+    }
+    break
+  case 'playlist-next':
+    if (playlist.playing) {
+      playlist.playNext()
+    }
+    break
+  default:
+    console.log('Error using command type in backend-music', args, playlist)
+    ChatMessage.create({ content: '[Macro backend-music] Passed command type not recognised' })
+    break
+}
---
+++ macros/playlist-control.js
@@ -0,0 +1,72 @@
+let availablePlaylists = ''
+let currentlyPlaying = ''
+let toChange;
+
+[...game.playlists].forEach((value, key) => {
+  availablePlaylists += `
+    <option value="${key}">
+      ${value.name} - ${value.data?.playing ? '▶️' : '⏸️'}
+    </option>`
+
+  if (value.data?.playing) {
+    let playing;
+    [...value.data.sounds].forEach((v) => {
+      if (v.data.playing) {
+        playing = v.data?.name
+      }
+    })
+    currentlyPlaying += `<li>${value.name} - ${playing}</li>`
+  }
+})
+
+new Dialog({
+  title: 'Playlist Control',
+  content: `
+    <form>
+      <div class="form-group">
+        <label>Select Playlist:</label>
+        <select id="playlist-selection" name="playlist-selection">${availablePlaylists}</select>
+      </div>
+      ${currentlyPlaying ? `
+        </br>
+        <div class="currently-playing">
+          Currently playing playlists & songs:
+          <ul>${currentlyPlaying}</ul>
+        </div>
+        ` : ''}
+    </form>`,
+  buttons: {
+    one: {
+      icon: '<i class="fas fa-play"></i>',
+      label: 'Toggle Playlist',
+      callback: () => { toChange = 'playlist-toggle' },
+    },
+    two: {
+      icon: '<i class="fas fa-forward"></i>',
+      label: 'Next Track',
+      callback: () => { toChange = 'playlist-next' },
+    },
+    three: {
+      icon: '<i class="fas fa-times"></i>',
+      label: 'Cancel',
+    },
+  },
+  default: 'Cancel',
+  close: (html) => {
+    if (toChange) {
+      const playlistIndex = html.find("select[name='playlist-selection']").val()
+      const playlist = game.playlists.contents[playlistIndex]
+
+      if (toChange === 'playlist-next' && playlist.playing === false) {
+        ui.notifications.error('Playlist isn\'t playing.')
+        return
+      }
+
+      if (playlist) {
+        ChatMessage.create({ content: `/backend-music ${toChange} "${playlist.name}"` })
+      } else {
+        ui.notifications.error('No valid playlist selected.')
+      }
+    }
+  },
+}).render(true)
